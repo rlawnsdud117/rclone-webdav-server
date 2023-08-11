725f08aeda58a9a5afc5f56faf82add3421f3f87
junyoung kim <38175394+rlawnsdud117@users.noreply.github.com>
2023-08-11 09:03:57 +0900

    Update rclone-webdav-server.sh

--- beta/1.0.0/rclone-webdav-server.sh
+++ beta/1.0.0/rclone-webdav-server.sh
@@ -12,39 +12,41 @@ else
   password="$2"
 fi
 if [ -z "$3" ]; then
+  bwlimit="0"
+else 
   bwlimit="$3"
 fi
 
-# rclone.conf 파일이 없는 경우 생성하도록 합니다.
-if [ ! -f /data/rclone.conf ]; then
+config_file=$"/data/config/rclone.conf"
+PORT=$"80"
+
+if [ ! -f $config_file ]; then
   if [ ! -f /root/.config/rclone/rclone.conf ]; then
-    echo "rclone.conf가 없습니다. 'rclone config'를 실행하여 구성하십시오!"
+    echo "rclone.conf is missing. Configure it by running 'rclone config'!"
     /bin/bash
   fi
-  mkdir -p /data
-  mv /root/.config/rclone/rclone.conf /data/rclone.conf 2>/dev/null
+  mkdir -p /data/config
+  mv /root/.config/rclone/rclone.conf /data/config/rclone.conf 2>/dev/null
 fi
-
-config_file=$"/data/rclone.conf"
-
-    section_name=$(awk 'NR==1 { if ($0 ~ /^\[[a-zA-Z0-9_-]+\]$/) print $0; else print "INVALID_SECTION_NAME" }' "$config_file")
-    if [ "$section_name" = "INVALID_SECTION_NAME" ]; then
-        echo "첫 번째 줄에서 유효한 섹션 이름을 찾지 못했습니다."
-        exit 1
-    fi
-
-    # [와 ] 문자 제거하여 섹션 이름만 추출
-    section_name=$(echo "$section_name" | sed 's/\[\(.*\)\]/\1/') 
-
 #/data/Log
+
 folder_path=$"/data/Log"
 if [ ! -d "$folder_path" ]; then
     mkdir -p "$folder_path"
 fi
 
-# Apache 웹 서버에서 WebDAV와 Basic Authentication 설정을 진행합니다.
+section_name=$(awk 'NR==1 { if ($0 ~ /^\[[a-zA-Z0-9_-]+\]$/) print $0; else print "INVALID_SECTION_NAME" }' "$config_file")
+
+if [ "$section_name" = "INVALID_SECTION_NAME" ]; then
+   echo "Failed to find valid section name in first line of file /data/config/rclone.conf."
+   exit 1
+fi
+
+section_name=$(echo "$section_name" | sed 's/\[\(.*\)\]/\1/') 
+
+
 rm -f /etc/apache2/webdav.password
 echo "$username:$(openssl passwd -apr1 $password)" > /etc/apache2/webdav.password
 
-rclone serve webdav $section_name: --addr 0.0.0.0:80 --config /data/rclone.conf --htpasswd /etc/apache2/webdav.password --etag-hash auto --vfs-cache-mode full --tpslimit 10 --tpslimit-burst 10 --dir-cache-time=160h --buffer-size=64M --vfs-read-chunk-size=2M --vfs-read-chunk-size-limit=2G --vfs-cache-max-age=5m --vfs-cache-mode=writes --log-file /data/Log/log.log --bwlimit $bwlimit 2>/dev/null
+rclone serve webdav $section_name: --port $PORT--config $config_file --htpasswd /etc/apache2/webdav.password --etag-hash auto --vfs-cache-mode full --tpslimit 10 --tpslimit-burst 10 --dir-cache-time=160h --buffer-size=64M --vfs-read-chunk-size=2M --vfs-read-chunk-size-limit=2G --vfs-cache-max-age=5m --vfs-cache-mode=writes --log-file /data/Log/log.log --bwlimit $bwlimit 2>/dev/null
 /bin/bash
