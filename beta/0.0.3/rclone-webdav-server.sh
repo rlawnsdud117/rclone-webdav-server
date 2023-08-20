7bf471c4b9e6079eb67bb571b55ffa0183943bc4
junyoung kim <38175394+rlawnsdud117@users.noreply.github.com>
2023-08-20 16:10:18 +0900

    Update rclone-webdav-server.sh

--- beta/0.0.3/rclone-webdav-server.sh
+++ beta/0.0.3/rclone-webdav-server.sh
@@ -44,10 +44,10 @@ if [ ! -f "$rclone_conf_destination" ]; then
     echo "rclone.conf가 없습니다. 'rclone config'를 실행하여 구성하십시오!"
     /bin/bash
   else 
-    echo "1. $rclone_conf_source 에서 $rclone_conf_destination 복사"
+    echo "1번 $rclone_conf_source 에서 $rclone_conf_destination 복사"
     cp -f "$rclone_conf_source" "$rclone_conf_destination"
   fi
 else
-  echo "2. $rclone_conf_source 에서 $rclone_conf_destination 복사"
-  cp -f "$rclone_conf_source" "$rclone_conf_destination"
+  echo "2번 $rclone_conf_source 에서 $rclone_conf_destination 복사"
+  cp -f "$rclone_conf_destination" "$rclone_conf_source"
 fi
