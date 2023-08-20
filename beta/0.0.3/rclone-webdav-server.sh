02cf76aa35d50051e6e89e4ad0a451fed390afb1
junyoung kim <38175394+rlawnsdud117@users.noreply.github.com>
2023-08-20 16:03:38 +0900

    Update rclone-webdav-server.sh

--- beta/0.0.3/rclone-webdav-server.sh
+++ beta/0.0.3/rclone-webdav-server.sh
@@ -33,20 +33,21 @@ fi
 config_folder=$"/data/config"
 rclone_conf_source=$"/root/.config/rclone/rclone.conf"
 rclone_conf_destination=$"/data/config/rclone.conf"
-echo "$config_folder \r\n $rclone_conf_source \r\n $rclone_conf_destination"
-if [ ! -f $rclone_conf_destination ]; then
-  if [ ! -f $rclone_conf_source ]; then
+
+echo "$config_folder"
+echo "$rclone_conf_source"
+echo "$rclone_conf_destination"
+
+
+if [ ! -f "$rclone_conf_destination" ]; then
+  if [ ! -f "$rclone_conf_source" ]; then
     echo "rclone.conf가 없습니다. 'rclone config'를 실행하여 구성하십시오!"
     /bin/bash
-    else 
-    echo ""$rclone_conf_source 에서 $rclone_conf_destination 복사"
-
-    cp -f "$rclone_conf_source $rclone_conf_destination" 
-  fi  
-  echo ""$rclone_conf_destination에서 $rclone_conf_source 복사"
-cp -f "$rclone_conf_destination $rclone_conf_source"
+  else 
+    echo "$rclone_conf_source 에서 $rclone_conf_destination 복사"
+    cp -f "$rclone_conf_source" "$rclone_conf_destination"
+  fi
 else
-    echo ""$rclone_conf_source 에서 $rclone_conf_destination 복사"
-
-cp -f "$rclone_conf_source $rclone_conf_destination"  
+  echo "$rclone_conf_source 에서 $rclone_conf_destination 복사"
+  cp -f "$rclone_conf_source" "$rclone_conf_destination"
 fi
