5e11434bd97cb6a749076f5e714210501ef8bf34
junyoung kim <38175394+rlawnsdud117@users.noreply.github.com>
2023-08-20 15:22:16 +0900

    Update rclone-webdav-server.sh

--- beta/0.0.3/rclone-webdav-server.sh
+++ beta/0.0.3/rclone-webdav-server.sh
@@ -39,11 +39,16 @@ if [ ! -f $rclone_conf_destination ]; then
     echo "rclone.conf가 없습니다. 'rclone config'를 실행하여 구성하십시오!"
     /bin/bash
     else
-    cp -f "$rclone_conf_source" "$rclone_conf_destination" 2>/dev/null0
+    cp -f "$rclone_conf_source" "$rclone_conf_destination" 
+    #2>/dev/null
   fi  
-cp -f "$rclone_conf_destination" "$rclone_conf_source" 2>/dev/null
+  echo ""$rclone_conf_destination에서 $rclone_conf_source 복사"
+cp -f "$rclone_conf_destination" "$rclone_conf_source"
+#2>/dev/null
 else
-cp -f "$rclone_conf_source" "$rclone_conf_destination"  2>/dev/null
+
+cp -f "$rclone_conf_source" "$rclone_conf_destination"  
+#2>/dev/null
 fi
 
 section_name=$(awk 'NR==1 { if ($0 ~ /^\[[a-zA-Z0-9_-]+\]$/) print $0; else print "INVALID_SECTION_NAME" }' "$rclone_conf_destination")
